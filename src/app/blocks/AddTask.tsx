7d780863ffb53747679c61ac0f169a320dc69348
Markus <markuslokk@gmail.com>
2024-04-14 20:37:38 +0300

    modify styles for add task, add useTransition for creating a task

--- src/app/blocks/AddTask.tsx
+++ src/app/blocks/AddTask.tsx
@@ -2,31 +2,34 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { createTask } from "@/db/queries";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, startTransition, useState, useTransition } from "react";
 
 const AddTask = () => {
+	const [isPending, startTransition] = useTransition();
 	const [value, setValue] = useState<string>("");
 
 	const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
 		setValue(e.target.value);
 	};
 
-	const handleAdd = async () => {
-		await createTask(value);
-		setValue("");
+	const handleAdd = () => {
+		startTransition(async () => {
+			await createTask(value);
+			setValue("");
+		});
 	};
 
 	return (
-		<div className="flex flex-row gap-4 place-content-center">
+		<div className="flex flex-row gap-4 place-content-center w-full max-w-md">
 			<Input
-				className="max-w-md w-full"
 				value={value}
 				onChange={handleInput}
 			/>
 			<Button
 				variant={"default"}
 				size={"sm"}
-				onClick={handleAdd}>
+				onClick={handleAdd}
+				disabled={isPending}>
 				Add Todo
 			</Button>
 		</div>
