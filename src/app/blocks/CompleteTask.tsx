7c3f7399b5d1f99d26447cc0dc70288f67aecacf
Markus <markuslokk@gmail.com>
2024-04-14 20:37:16 +0300

    change button icon depending on isChecked status

--- src/app/blocks/CompleteTask.tsx
+++ src/app/blocks/CompleteTask.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { completeTask } from "@/db/queries";
-import { Check } from "lucide-react";
+import { Check, Undo2 } from "lucide-react";
 import { useTransition } from "react";
 
-const CompleteTask = ({ taskId }: { taskId: number }) => {
+const CompleteTask = ({ taskId, isChecked }: { taskId: number; isChecked: boolean }) => {
 	const [isPending, startTransition] = useTransition();
 
 	const handleComplete = () => {
@@ -15,11 +15,11 @@ const CompleteTask = ({ taskId }: { taskId: number }) => {
 
 	return (
 		<Button
-			variant={"default"}
+			variant={isChecked ? "outline" : "default"}
 			size={"icon"}
 			onClick={handleComplete}
 			disabled={isPending}>
-			<Check />
+			{isChecked ? <Undo2 /> : <Check />}
 		</Button>
 	);
 };
