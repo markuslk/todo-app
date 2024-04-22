87c61e2fb5aa717fe2627f723db87fb7504e89e4
Markus <markuslokk@gmail.com>
2024-04-22 12:33:43 +0300

    update schema, handle login, log out and user creation actions

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,8 +1,13 @@
 "use server";
-import { asc, desc, eq, not } from "drizzle-orm";
+
+import { SignInSchema, SignupFormSchema } from "@/lib/lib";
+import { InferInsertModel, and, asc, desc, eq, not } from "drizzle-orm";
 import db from "./drizzle";
-import { tasks } from "./schema";
+import { tasks, users } from "./schema";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import bcrypt from "bcrypt";
+import { createSession, deleteSession } from "@/lib/session";
 
 export const getAllTasks = async () => {
 	const data = await db.query.tasks.findMany({
@@ -18,8 +23,9 @@ export const getTask = async (taskId: number) => {
 
 	return data;
 };
-export const createTask = async (taskName: string) => {
-	await db.insert(tasks).values({ name: taskName });
+
+export const createTask = async (taskName: string, userId: number) => {
+	await db.insert(tasks).values({ name: taskName, userId: userId });
 	revalidatePath("/");
 };
 
@@ -35,3 +41,81 @@ export const deleteTask = async (taskId: number) => {
 	await db.delete(tasks).where(eq(tasks.id, taskId));
 	revalidatePath("/");
 };
+
+export type CreateUserProps = InferInsertModel<typeof users>;
+
+export const createUser = async (formData: FormData) => {
+	const validateFields = SignupFormSchema.safeParse({
+		email: formData.get("email"),
+		name: formData.get("name"),
+		password: formData.get("password"),
+	});
+
+	if (!validateFields.success) {
+		validateFields.error;
+		console.log(validateFields.error);
+	} else {
+		const { email, name, password } = validateFields.data;
+
+		const existingUser = await db.query.users.findFirst({
+			where: eq(users.email, email),
+		});
+
+		if (existingUser) {
+			return {
+				message: "Email already exists.",
+			};
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		await db.insert(users).values({
+			email: email,
+			name: name,
+			password: hashedPassword,
+		});
+		redirect("/sign-in");
+	}
+};
+
+export async function logout() {
+	deleteSession();
+	redirect("/sign-in");
+}
+
+export async function login(formData: FormData) {
+	const validateFields = SignInSchema.safeParse({
+		email: formData.get("email"),
+		password: formData.get("password"),
+	});
+
+	if (!validateFields.success) {
+		validateFields.error;
+		console.log(validateFields.error);
+	} else {
+		const { email, password } = validateFields.data;
+
+		// const passwordMatches = await bcrypt.compare(password, users.password);
+
+		// console.log(email, hashedPassword);
+		// $2b$10$16sl1GNIvr9Oh97zmJxXn.7v9fMVqmDxkxIhRgNap5LCu/Vd90cZa
+		// $2b$10$k5VxDWtgRqyTn3ynZNFstuw5BsaIzhBMhkvolVVtVi9ciz9GH6TEG
+		// $2b$10$MZMcXfWk8c1ODm.wXop.GOVxLsm9uOJlq8wums.Y7X2jmVt0VgAJ.
+
+		const user = await db.query.users.findFirst({
+			where: eq(users.email, email),
+		});
+
+		console.log(user);
+
+		if (!user) {
+			return {
+				message: "Something went wrong with authenticating users",
+			};
+		}
+		if (user) {
+			createSession(user.id);
+			redirect("/");
+		}
+	}
+}
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,34 @@
-import { boolean, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { boolean, integer, pgTable, serial, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+	"users",
+	{
+		id: serial("id").primaryKey(),
+		name: text("name").notNull(),
+		email: text("email").notNull().unique(),
+		password: text("password").notNull(),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+	},
+	(users) => ({
+		emailIndex: uniqueIndex("email_idx").on(users.email),
+	})
+);
 
 export const tasks = pgTable("tasks", {
 	id: serial("id").primaryKey(),
 	name: text("task_name").notNull(),
 	isChecked: boolean("is_checked").notNull().default(false),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+	userId: integer("user_id")
+		.notNull()
+		.references(() => users.id),
 });
+
+export const taskRelations = relations(tasks, ({ one, many }) => ({
+	user: one(users, { fields: [tasks.userId], references: [users.id] }),
+}));
+
+export const usersRelations = relations(users, ({ many }) => ({
+	tasks: many(tasks),
+}));
