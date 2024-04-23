0f2c90764b95e7a6219803be35c5a819c2ad5c43
Markus <markuslokk@gmail.com>
2024-04-24 01:28:37 +0300

    refactor schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,19 +1,13 @@
 import { relations } from "drizzle-orm";
 import { boolean, integer, pgTable, serial, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
 
-export const users = pgTable(
-	"users",
-	{
-		id: serial("id").primaryKey(),
-		name: text("name").notNull(),
-		email: text("email").notNull().unique(),
-		password: text("password").notNull(),
-		createdAt: timestamp("created_at").notNull().defaultNow(),
-	},
-	(users) => ({
-		emailIndex: uniqueIndex("email_idx").on(users.email),
-	})
-);
+export const users = pgTable("users", {
+	id: serial("id").primaryKey(),
+	name: text("name").notNull(),
+	email: text("email").notNull().unique(),
+	password: text("password").notNull(),
+	createdAt: timestamp("created_at").notNull().defaultNow(),
+});
 
 export const tasks = pgTable("tasks", {
 	id: serial("id").primaryKey(),
@@ -21,11 +15,11 @@ export const tasks = pgTable("tasks", {
 	isChecked: boolean("is_checked").notNull().default(false),
 	createdAt: timestamp("created_at").notNull().defaultNow(),
 	userId: integer("user_id")
-		.notNull()
-		.references(() => users.id),
+		.references(() => users.id)
+		.notNull(),
 });
 
-export const taskRelations = relations(tasks, ({ one, many }) => ({
+export const taskRelations = relations(tasks, ({ one }) => ({
 	user: one(users, { fields: [tasks.userId], references: [users.id] }),
 }));
 
