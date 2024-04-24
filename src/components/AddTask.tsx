ca34f2549d2d57aedf2d8c7859e871f8fa721a57
Markus <markuslokk@gmail.com>
2024-04-24 13:56:41 +0300

    add keyboard event to submit input with enter key

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { createTask } from "@/db/queries";
-import { ChangeEvent, useState, useTransition } from "react";
+import { ChangeEvent, KeyboardEvent, useState, useTransition } from "react";
 
 const AddTask = ({ userId }: { userId: number }) => {
 	const [isPending, startTransition] = useTransition();
@@ -19,15 +19,21 @@ const AddTask = ({ userId }: { userId: number }) => {
 		});
 	};
 
+	const handleEnterClick = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter") handleAdd();
+	};
+
 	return (
 		<div className="flex flex-row gap-4 place-content-center w-full max-w-md">
 			<Input
 				value={value}
 				onChange={handleInput}
+				onKeyDown={handleEnterClick}
 			/>
 			<Button
 				variant={"default"}
 				size={"sm"}
+				type="submit"
 				onClick={handleAdd}
 				disabled={isPending}>
 				Add Todo
